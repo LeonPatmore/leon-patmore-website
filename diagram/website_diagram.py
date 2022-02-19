5f5cdcc16deea4e99fff8182caca56029905ff0e
Leon Patmore <leon.patmore@bath.edu>
2022-02-19 15:48:16 +0000

    Making diagram a bit clearer.

--- diagram/website_diagram.py
+++ diagram/website_diagram.py
@@ -9,28 +9,32 @@ font_colour = "black"
 
 diagram_attributes = {
     "pad": "0.1",
-    "fontsize": "45",
-    "fontcolor": font_colour,
+    "fontsize": "50",
+    "fontcolor": "white",
     "bgcolor": "transparent",
+}
+
+cluster_attributes = {
+    "fontcolor": "black",
+    "fontsize": "20"
 }
 
 with Diagram("Website Design",
              graph_attr=diagram_attributes,
-             node_attr={"fontsize": "18", "fontcolor": font_colour},
              filename="my_website"):
     mobile = Mobile()
-    with Cluster("CV"):
+    with Cluster("CV", graph_attr=cluster_attributes):
         cv_store = S3()
-    with Cluster("Images"):
+    with Cluster("Images", graph_attr=cluster_attributes):
         image_store = S3()
-    with Cluster("Lambda Function"):
+    with Cluster("Lambda Function", graph_attr=cluster_attributes):
         fuc = LambdaFunction()
-        nuxt = Custom(label="Nuxt", icon_path="./nuxt.png")
+        nuxt = Custom(label="", icon_path="./nuxt.png")
         fuc >> Edge(style="dotted", color="darkgreen") >> nuxt
-    with Cluster("API GW"):
+    with Cluster("API GW", graph_attr=cluster_attributes):
         api_gw = APIGateway()
     api_gw >> Edge(color="black") >> fuc
-    with Cluster("leonpatmore.com"):
+    with Cluster("leonpatmore.com", graph_attr=cluster_attributes):
         r53 = Route53()
     mobile >> Edge(color="black") >> r53 >> Edge(color="black") >> api_gw
     cv_store << Edge(color="black") << mobile
