7df2010b8fe8716afe4efe5e7f0836170b1e7395
Leon Patmore <leon.patmore@bath.edu>
2022-02-20 20:09:44 +0000

    updating index.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,20 +2,20 @@
     <div>
         <span class="bg"></span>
         <v-container>
-            <v-row>
+            <v-row class="no-gutters">
                 <v-col cols="12">
                     <v-card outlined color="transparent">
                         <v-card-title class="justify-center">
-                            <div class="text-md-h1 title-color mt-10 text-h3">
+                            <div class="text-md-h1 title-color mt-10 text-h2">
                                 Leon Patmore
                             </div>
                         </v-card-title>
                     </v-card>
                 </v-col>
             </v-row>
-            <v-row class="no-gutters">
+            <v-row class="no-gutters pb-md-8">
                 <v-col cols="12">
-                    <v-card outlined color="transparent" class="pa-5 pt-0">
+                    <v-card outlined color="transparent">
                         <v-card-text
                             class="text-center text-subtitle-1 black--text"
                             >Hello and welcome to my website! Please find my CV
@@ -24,32 +24,29 @@
                     </v-card>
                 </v-col>
             </v-row>
-            <v-row class="justify-center no-gutters">
+            <v-row class="justify-center no-gutters pb-md-8">
                 <v-col :lg="8" :md="10" cols="12">
-                    <v-card outlined color="transparent" class="pa-5">
+                    <v-card class="pa-5 pt-0" color="rgb(0, 0, 0, 0.0)">
                         <v-img
-                            max-width="100%"
+                            max-width="1000px"
                             src="https://leon-patmore-cv.s3.eu-west-2.amazonaws.com/my_website.png"
                         />
                     </v-card>
                 </v-col>
             </v-row>
             <v-row class="no-gutters">
-                <v-col cols="12">
-                    <v-card
-                        outlined
-                        color="transparent"
-                        class="pa-5 d-flex justify-center"
-                    >
-                        <v-img
-                            v-for="img in images"
-                            v-bind:key="img"
-                            class="ma-1"
-                            max-width="100"
-                            contain
-                            :src="img"
-                        ></v-img>
-                    </v-card>
+                <v-col
+                    v-for="img in images"
+                    v-bind:key="img"
+                    class="d-flex justify-center align-center"
+                >
+                    <v-img
+                        class="ma-1"
+                        min-width="70"
+                        width="6vh"
+                        max-width="150"
+                        :src="img"
+                    ></v-img>
                 </v-col>
             </v-row>
         </v-container>
